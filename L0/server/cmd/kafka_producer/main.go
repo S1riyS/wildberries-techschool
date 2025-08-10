cc57edcfb28d2c8804bd34866f950ed708b80166
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-08-10 13:53:36 +0500

    ✨ feat: add kafka order producer as separate cmd

--- L0/server/cmd/kafka_producer/main.go
+++ L0/server/cmd/kafka_producer/main.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/S1riyS/wildberries-techschool/L0/server/internal/config"
+	"github.com/S1riyS/wildberries-techschool/L0/server/internal/domain"
+	"github.com/S1riyS/wildberries-techschool/L0/server/internal/infrastructure/kafka"
+	"github.com/go-faker/faker/v4"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Read flags
+	var envPath string
+	flag.StringVar(&envPath, "env-path", "", "Path to .env file")
+	flag.Parse()
+
+	// Load local .env file in dev mode
+	err := godotenv.Load(envPath)
+	if err != nil {
+		panic(fmt.Errorf("error loading .env file: %v", err))
+	}
+	log.Println("Loaded .env file")
+
+	// Init config
+	cfg := config.MustNew()
+	log.Println("Loaded config")
+
+	// Create order producer
+	orderProducer, err := kafka.NewProducer(cfg.Kafka.Brokers)
+	if err != nil {
+		panic(fmt.Errorf("error creating order producer: %v", err))
+	}
+	log.Println("Created order producer")
+	defer orderProducer.Close()
+
+	// Generate order
+	var randomOrder domain.Order
+	err = faker.FakeData(&randomOrder)
+	if err != nil {
+		panic(fmt.Errorf("error generating order: %v", err))
+	}
+	orderJSON, err := json.Marshal(randomOrder)
+	if err != nil {
+		panic(fmt.Errorf("error marshaling order to json: %v", err))
+	}
+	log.Println("Created order JSON")
+
+	err = orderProducer.Produce(string(orderJSON), cfg.Kafka.Topic, "")
+	if err != nil {
+		panic(fmt.Errorf("error producing order: %v", err))
+	}
+}
