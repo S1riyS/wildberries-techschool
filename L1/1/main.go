803e06aeae73f999b4ef6e147f03d604a3885cb9
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-08-13 13:17:37 +0500

    ✨ feat(L1): add L1.1 solution

---
+++ L1/1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// Parent structure - Human
+type Human struct {
+	Name string
+	Age  int
+}
+
+// Human' method
+func (h *Human) SayHello() {
+	fmt.Printf("Hello, my name is %s and I'm %d years old.\n", h.Name, h.Age)
+}
+
+// Child structure - Actions. Human is embedded
+type Action struct {
+	Human
+	Profession string
+}
+
+// Action's method
+func (a *Action) Work() {
+	fmt.Printf("%s works as %s.\n", a.Name, a.Profession)
+}
+
+func main() {
+	person := Action{
+		Human: Human{
+			Name: "Kirill",
+			Age:  20,
+		},
+		Profession: "Go developer",
+	}
+
+	// Call Human's method
+	person.SayHello() // Метод Human доступен через Action
+
+	// Call Action's method
+	person.Work()
+
+	// Access fields
+	fmt.Println("Name:", person.Name)
+	fmt.Println("Age:", person.Age)
+	fmt.Println("Profession:", person.Profession)
+}
