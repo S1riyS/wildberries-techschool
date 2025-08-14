54813b9122d883831909a02bd40e4750a9c0a089
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-08-14 17:24:31 +0500

    ✨ feat(L1): add L1.11 solution

---
+++ L1/11/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Find intersection of two arrays
+//
+// Time complexity: O(n + m)
+func intersect(a, b []int) []int {
+	counter := make(map[int]int, len(a))
+	for _, v := range a {
+		counter[v]++
+	}
+
+	var result []int
+	for _, v := range b {
+		if counter[v] > 0 {
+			result = append(result, v)
+			counter[v]--
+		}
+	}
+	return result
+}
+
+func main() {
+	a := []int{1, 2, 3, 3}
+	b := []int{2, 3, 3, 4}
+	fmt.Println(intersect(a, b))
+}
